9131457c399e2c3733a394331b8bf43fbcbfb2b9
Daniel Correa Tucunduva <daniel.correatucunduva@cgi.com>
2019-03-21 14:08:36 -0400

    Expose API with hardcoded token

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -7,21 +7,27 @@ const jwksRsa = require('jwks-rsa')
 // Authentication middleware. When used, the
 // Access Token must exist and be verified against
 // the Auth0 JSON Web Key Set
-const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the signing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://sprint-ng.auth0.com/.well-known/jwks.json`
-  }),
+const checkJwt = (req, res, next) => {
+  const token = req.get('Token')
+  if (token && token === '23ry8347yr') {
+    return next()
+  }
+  return jwt({
+    // Dynamically provide a signing key
+    // based on the kid in the header and
+    // the signing keys provided by the JWKS endpoint.
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://sprint-ng.auth0.com/.well-known/jwks.json`
+    }),
 
-  audience: 'https://sprint-ng.auth0.com/api/v2/',
-  issuer: `https://sprint-ng.auth0.com/`,
-  algorithms: ['RS256']
-})
+    audience: 'https://sprint-ng.auth0.com/api/v2/',
+    issuer: `https://sprint-ng.auth0.com/`,
+    algorithms: ['RS256']
+  })
+}
 
 var pastSprints = require('./api/past-sprints.route')
 var sprintTemplates = require('./api/sprint-templates.route')
