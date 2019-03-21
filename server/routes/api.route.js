ab6b5f38ae5011d15303ebc250f43b2f364f23bf
Daniel Correa Tucunduva <daniel.correatucunduva@cgi.com>
2019-03-21 17:37:55 -0400

    Config token

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -7,27 +7,29 @@ const jwksRsa = require('jwks-rsa')
 // Authentication middleware. When used, the
 // Access Token must exist and be verified against
 // the Auth0 JSON Web Key Set
-const checkJwt = (req, res, next) => {
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://sprint-ng.auth0.com/.well-known/jwks.json`
+  }),
+
+  audience: 'https://sprint-ng.auth0.com/api/v2/',
+  issuer: `https://sprint-ng.auth0.com/`,
+  algorithms: ['RS256']
+})
+
+const checkToken = (req, res, next) => {
   const token = req.get('Token')
   if (token && token === process.env.TOKEN) {
     next()
     return
   }
-  jwt({
-    // Dynamically provide a signing key
-    // based on the kid in the header and
-    // the signing keys provided by the JWKS endpoint.
-    secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: `https://sprint-ng.auth0.com/.well-known/jwks.json`
-    }),
-
-    audience: 'https://sprint-ng.auth0.com/api/v2/',
-    issuer: `https://sprint-ng.auth0.com/`,
-    algorithms: ['RS256']
-  })
+  next(new Error('Invalid token'))
 }
 
 const pastSprints = require('./api/past-sprints.route')
@@ -38,6 +40,10 @@ const tokens = require('./api/tokens.route')
 router.use('/past-sprints', checkJwt, pastSprints)
 router.use('/sprint-templates', checkJwt, sprintTemplates)
 router.use('/users', checkJwt, users)
+
 router.use('/token', tokens)
+router.use('/token/past-sprints', checkToken, pastSprints)
+router.use('/token/sprint-templates', checkToken, sprintTemplates)
+router.use('/token/users', checkToken, users)
 
 module.exports = router
