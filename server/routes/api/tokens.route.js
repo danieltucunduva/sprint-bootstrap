97754ac28a0a70a7b23312aee0add210fa5f30bf
Daniel Correa Tucunduva <daniel.correatucunduva@cgi.com>
2019-03-21 17:21:08 -0400

    Config token

--- server/routes/api/tokens.route.js
+++ server/routes/api/tokens.route.js
@@ -2,6 +2,6 @@ var express = require('express')
 var router = express.Router()
 var tokensController = require('../../controllers/tokens.controller')
 
-router.get('/token', tokensController.getToken)
+router.get('/', tokensController.getToken)
 
 module.exports = router
